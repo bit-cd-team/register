8e921d01bd8ab174924bf7fff94e3207fb82bb15
suwenzheng <15589721080@163.com>
2020-10-14 18:43:43 +0800

    创建注册中心

---
+++ src/main/java/com/swz/register/RegisterCenterApplication.java
@@ -0,0 +1,13 @@
+package com.swz.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swz/register/RegisterCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swz.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
